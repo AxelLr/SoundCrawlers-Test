f6f41c8fad7ef22e5a3d478286106a8f6a5506bd
Axel <axelleonr12@gmail.com>
2020-07-02 00:05:03 -0300

    Updates

---
+++ precache-manifest.e106b0638867226eb650fdcb2fe02173.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23aba63ad05bc65b5a2b4645ffdea5c5",
+    "url": "/SoundCrawlers-Test/index.html"
+  },
+  {
+    "revision": "366f92b71e5ab32f9784",
+    "url": "/SoundCrawlers-Test/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "767dc400117a34be8510",
+    "url": "/SoundCrawlers-Test/static/css/main.87ffec6f.chunk.css"
+  },
+  {
+    "revision": "366f92b71e5ab32f9784",
+    "url": "/SoundCrawlers-Test/static/js/2.f7eff1c6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SoundCrawlers-Test/static/js/2.f7eff1c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "767dc400117a34be8510",
+    "url": "/SoundCrawlers-Test/static/js/main.ca85c2f2.chunk.js"
+  },
+  {
+    "revision": "e5a0f4c2cb56d2fff9a5",
+    "url": "/SoundCrawlers-Test/static/js/runtime-main.c46b6b69.js"
+  },
+  {
+    "revision": "612942adc93ee2d9d69d524ab0ac33f1",
+    "url": "/SoundCrawlers-Test/static/media/SearchIcon.612942ad.svg"
+  },
+  {
+    "revision": "ffb76979b8878284f2383950ac5d30bb",
+    "url": "/SoundCrawlers-Test/static/media/close-icon.ffb76979.svg"
+  },
+  {
+    "revision": "b2d23c0dbd08ac43569ff7ff72531a49",
+    "url": "/SoundCrawlers-Test/static/media/comment-icon.b2d23c0d.svg"
+  },
+  {
+    "revision": "9391826c4268ad9b2cbf6257d69fc77d",
+    "url": "/SoundCrawlers-Test/static/media/flag-icon.9391826c.svg"
+  },
+  {
+    "revision": "6bd481ee81820733132873035b4ac88e",
+    "url": "/SoundCrawlers-Test/static/media/headphones-icon.6bd481ee.svg"
+  },
+  {
+    "revision": "8210b7f5998d899095bd634b0be40847",
+    "url": "/SoundCrawlers-Test/static/media/like-icon-2.8210b7f5.svg"
+  },
+  {
+    "revision": "ae7b1122e73905fa99d0f358a2a3267e",
+    "url": "/SoundCrawlers-Test/static/media/like-icon.ae7b1122.svg"
+  },
+  {
+    "revision": "68b4d1fbdc55f579eebe19ad9760e512",
+    "url": "/SoundCrawlers-Test/static/media/profileImage.68b4d1fb.svg"
+  },
+  {
+    "revision": "941f9e31b34701f883699742b20b94f4",
+    "url": "/SoundCrawlers-Test/static/media/track-icon.941f9e31.svg"
+  },
+  {
+    "revision": "bdc89419a752df2242d70821d5e5b900",
+    "url": "/SoundCrawlers-Test/static/media/youtubeVideo.bdc89419.png"
+  }
+]);
